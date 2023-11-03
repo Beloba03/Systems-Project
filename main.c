b49f6546ce48607ab246e0711adc36df629c7295
Beloba03 <benbab03@gmail.com>
2023-11-03 01:55:18 -0300

    Tidied up the code

--- main.c
+++ main.c
@@ -1,46 +1,62 @@
-// Include the building generator header
+// Include the building generator header.
 #include "BldgGen.h"
 
-// Main function
+// Main function to demonstrate the building generator.
 int main(int argc, char *argv[]) {
-    // Check if the user has provided the correct number of arguments
+    
+    // Check if the user has provided the correct number of arguments.
     if (argc != 2) {
         printf("Supply name of data file\n");
-        (void) getchar();  // Wait for a character input before exiting
-        return 1;  // Return an error code
+        (void) getchar(); // Wait for a character input before exiting.
+        return 1;        // Return an error code.
     }
 
-    // Try to open the provided file for reading
-    if ((bfd = fopen(argv[1], "r")) == NULL) {
+    // Attempt to open the provided data file for reading.
+    bfd = fopen(argv[1], "r");
+    if (bfd == NULL) {
         printf("Can't open %s\n", argv[1]);
-        getchar();  // Wait for a character input before exiting
-        return 1;  // Return an error code
+        getchar();      // Wait for a character input before exiting.
+        return 1;       // Return an error code.
     }
-    // Read the file and set up the city grid layout
+
+    // Read the data from the file and set up the city grid layout.
     read_file();
+    
+    // Prepare the console for the animation.
     hideCursor();
-    setConsoleBufferSizeAndWindow(300, 300, 80, 50); // 80 columns wide, 200 rows buffer, 50 rows visible in window
-    // Animate the car's movement on the console
+    setConsoleBufferSizeAndWindow(300, 300, 80, 50); // Set buffer and window sizes.
+
+    // Variables to track the car's movement on the console.
     int destStatus = 0, wasDKeyPressed = 0;
     while (1) {
-    int isDKeyPressed = GetAsyncKeyState('D') & 0x8000;
+        // Check if the 'D' key is pressed.
+        int isDKeyPressed = GetAsyncKeyState('D') & 0x8000;
 
-    if (isDKeyPressed && !wasDKeyPressed) {  // Key just got pressed
-        updateEndCoordinates();  // Prompt user for new destination
+        // If the 'D' key was just pressed, update the car's destination coordinates.
+        if (isDKeyPressed && !wasDKeyPressed) {
+            updateEndCoordinates();
+        }
+
+        // Update the state of the 'D' key for the next iteration.
+        wasDKeyPressed = isDKeyPressed;
+
+        // Animate each car on the grid.
+        for (int i = 0; i < numCars; i++) {
+            animateCar(i);
+        }
+
+        // Introduce a delay to make the animation smoother.
+        Sleep(50);  
     }
 
-    wasDKeyPressed = isDKeyPressed;  // Update the state for the next iteration
-    for(int i = 0; i < numCars; i++)
-    {
-        animateCar(i);
-    }
-    
-    Sleep(50);  
-}
-    // Set the cursor position to the bottom of the grid
+    // Set the cursor position to the bottom of the grid.
     setCursorPosition(0, 4*ybldg+7);
-    printf("\nDone\n");  // Print the "Done" message
-    (void) getchar();  // Wait for a character input before exiting
+    printf("\nDone\n");  // Inform the user that the animation is complete.
+
+    (void) getchar();    // Wait for a character input before exiting.
+
+    // Clean up resources.
     freeGrid(ybldg);
-    return 0;  // Return a success code
+
+    return 0; // Return a success code.
 }
